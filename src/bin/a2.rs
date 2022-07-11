625e70bba75ca983a9bc7a04344ca7562d588d3f
jack hanna <jack_hanna1@hotmail.com>
2022-07-11 16:14:30 +0200

    pushing udemy ex for the first time

---
+++ src/bin/a2.rs
@@ -0,0 +1,23 @@
+// Topic: Basic arithmetic
+//
+// Program requirements:
+// * Displays the result of the sum of two numbers
+//
+// Notes:
+// * Use a function to add two numbers together
+fn add_num(a:i32, b:i32) -> i32 {
+  a + b
+}
+
+// * Use a function to display the result
+fn display_result(result: i32) {
+  println!("{:?}", result);
+}
+
+
+// * Use the "{:?}" token in the println macro to display the result
+
+fn main() {
+  let result = add_num(2, 2);
+  display_result(result);
+}
